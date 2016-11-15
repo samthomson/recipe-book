350770928553c2b19b12b10ce0cbc150d8a5d18f
unknown <samt@samt.st>
2016-11-15 22:25:35 +0100

    can't get lazy loading modules to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,44 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 import { DropdownDirective } from './dropdown.directive';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 import { routing } from "./app.routing";
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start.component';
 import { HttpModule } from '@angular/http';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home.component';
+
+
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeService } from "./recipes/recipe.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     DropdownDirective,
-    RecipeEditComponent,
-    RecipeStartComponent
+    HomeComponent,
+    ShoppingListComponent
   ],
   imports: [
 	  FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     HttpModule,
-    ReactiveFormsModule,
     routing
   ],
-  providers: [ShoppingListService, ShoppingListModule],
+  providers: [ShoppingListService, ShoppingListModule, RecipeService],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
